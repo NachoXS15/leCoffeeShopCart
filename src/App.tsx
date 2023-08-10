db81cbe6598ebe8809a218371771e1bbd203e5dc
NachoXS15 <86374911+NachoXS15@users.noreply.github.com>
2023-08-10 17:56:19 -0300

    implemented routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Header from "./components/Header";
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Order from './pages/Order';
+import { Container } from '@mui/material';
+import Header from './components/Header';
 function App() {
   return (
+    <>
     <Header />
-  );
+      <section>
+        <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/order' element={<Order />}/>
+        </Routes>
+      </section>
+    </>
+  )
 }
 
 export default App;
---
+++ src/components/PickProducts.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function PickProducts() {
+  return (
+    <section>
+        <div>
+            <h2 id='header-text'>PRODUCTOS DISPONIBLES</h2>
+            <div>
+                
+            </div>
+        </div>
+
+    </section>
+  )
+}
