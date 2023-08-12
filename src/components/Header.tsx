288d7786e2fdffacb32b152978727afc5c83eaf9
NachoXS15 <86374911+NachoXS15@users.noreply.github.com>
2023-08-11 22:37:40 -0300

    updated header styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,27 @@
 import React from 'react'
 import "../styles/styles.scss"
 import Logo from "../assets/img/logo.png"
+import { NavLink } from 'react-router-dom'
+import { Button, Nav } from 'react-bootstrap'
 export default function Header() {
   return (
     <header>
+      <div>
         <a href="#"><img src={Logo} alt="" width={90} /></a>
-        <h3>CARGA Y GENERACION DE PEDIDOS</h3>
+        <h4>CARGA Y GENERACION DE PEDIDOS</h4>
+      </div>
+      <nav className='nav'>
+        <Nav.Link to="/" as={NavLink}>
+          Home
+        </Nav.Link>
+        <Nav.Link to="/store" as={NavLink}>
+          Store
+        </Nav.Link>
+        <Button style={{position: 'relative'}}>
+          Cart
+          <div className='notify'>3</div>
+        </Button>
+      </nav>
     </header>
   )
 }
